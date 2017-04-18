578db1b1a4f117f6be074f5d57280d71a563d9a9
Manuel Alabor <manuel@alabor.me>
2017-04-18 17:42:36 +0200

    fix(export): Reexport everything from main file
    #1

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from './src/parsers';
+export * from './src/parsers/array';
+export * from './src/parsers/boolean';
+export * from './src/parsers/map';
+export * from './src/parsers/number';
+export * from './src/parsers/string';
