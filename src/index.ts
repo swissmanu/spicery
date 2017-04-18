a8c3c72df85b7e38acfa55bbabad7cd2f0a5e182
Manuel Alabor <manuel@alabor.me>
2017-04-18 19:18:13 +0200

    fix(typescript): Fix TypeScript Build
    Ensure there is really a build executed and package.json points to the built files.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './parsers';
+export * from './parsers/array';
+export * from './parsers/boolean';
+export * from './parsers/map';
+export * from './parsers/number';
+export * from './parsers/string';
